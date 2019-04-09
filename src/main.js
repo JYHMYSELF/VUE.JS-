5ea068d181d601858fb2de676e3a2e1f9fafbe05
JYHMYSELF <382500433@qq.com>
2019-04-09 16:53:50 +0800

    enter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 // 入口文件
 import Vue from 'vue'
 
+//1.1导入路由的包
+import VueRouter from 'vue-router'
+//1.2安装路由
+Vue.use(VueRouter)
+//1.3 导入自己的router.js路由模块
+import router from './router.js'
+
 import app from './App.vue'
 
 // 导入 MUI 的样式
@@ -16,5 +23,7 @@ Vue.component(Header.name, Header)
 
 var vm = new Vue({
     el: '#app',
-    render: c => c(app)
+    render: c => c(app),
+    //1.4挂载路由对象到VM实例上
+    router: router
 })
--- src/router.js
+++ src/router.js
@@ -3,8 +3,9 @@ import VueRouter from 'vue-router'
 
 // 3. 创建路由对象
 var router = new VueRouter({
-  routes: [
-  ]
+  routes: [],
+  //覆盖默认的路由高亮的类，默认的类叫做router-link-active
+  linkActiveClass: 'mui-active'
 })
 
 // 把路由对象暴露出去
